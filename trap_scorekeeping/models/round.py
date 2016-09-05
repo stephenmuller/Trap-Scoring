d77d5420bebec454aa0dfb87ceff9bf4fec3d02f
Stephen <stevemuller@gmail.com>
2016-09-05 16:04:06 -0700

    updated a bit

--- trap_scorekeeping/models/round.py
+++ trap_scorekeeping/models/round.py
@@ -8,13 +8,14 @@ from django.db import models
 
 
 
-class Round:
+class Round(models.Model):
     """Links together the various classes/information for one round"""
-    ssingles_round = SinglesScore()
+    singles_round = SinglesScore()
     player = player  # stores user specific data that is mostly static
     date = models.DateTimeField(auto_now_add=True)  # can be used to pull weather later
     starting_station = 1  # the default station is one, small detail only useful for statistics
     location = 'Portland Gun Club' # static for now, eventually will use the day class for this information
+    shells = shells class
 
     def __eq__(self, other):
         return(
