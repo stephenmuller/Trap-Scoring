58322c071450a7f4f07db000e4097b7da461ad02
Stephen <stevemuller@gmail.com>
2016-09-09 16:30:30 -0700

    end of day

--- trap_scorekeeping/logic.py
+++ trap_scorekeeping/logic.py
@@ -1,6 +1,9 @@
 """trap_scorekeeping Logic."""
 
 from django.db import migrations, models
+from . import models
+from django.db.models import Q
+
 
 def last_five_rounds():
     """querys for the last five rounds and returns a list"""
