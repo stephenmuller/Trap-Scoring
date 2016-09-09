a2330fbf85737a51bf387cbc854cff7c772fd570
Stephen <stevemuller@gmail.com>
2016-09-09 16:21:05 -0700

    adding admin link?

--- trap_scorekeeping/logic.py
+++ trap_scorekeeping/logic.py
@@ -1,1 +1,7 @@
 """trap_scorekeeping Logic."""
+
+from django.db import migrations, models
+
+def last_five_rounds():
+    """querys for the last five rounds and returns a list"""
+    return Round.objects.all()[::-1][:5]
