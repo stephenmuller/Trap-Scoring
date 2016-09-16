2878ce1bab98e7db38b088e0ee6d5e4d87286075
Stephen <stevemuller@gmail.com>
2016-09-16 14:30:50 -0700

    updates

--- trap_scorekeeping/views.py
+++ trap_scorekeeping/views.py
@@ -5,21 +5,6 @@ from . import logic
 from . import models
 from django.contrib.auth.models import User
 
-#
-# def clean_up_round_for_display(round):
-#     """takes in a round object and prettyfies it
-#
-#     >>> logic.create_user('test', 'test', 'test')
-#     >>> logic.create_new_gun_model('beretta', 'a400', '12', 28, 'shell catcher')
-#     >>> logic.create_new_shells_model('remmington', 'gameloads', '7.5', '1oz', 1290)
-#     >>> logic.create_new_round(User.objects.get(id=1), 24, '1997-07-16T19:20:30+01:00',
-#     ... 'portland gun club', models.Shotgun.objects.get(id=1), models.Shells.objects.get(id=1), '1',
-#     ... 'no excuses!!')
-#     >>> models.Round.objects.all()[0]
-#     Round(<User: test>, SinglesScore('a', False), datetime.datetime(1997, 7, 16, 18, 20, 30, tzinfo=<UTC>), 'portland gun club', Shotgun('beretta', 'a400', '12', 28, 'shell catcher'), Shells('remmington', 'gameloads', '7.5', '1oz', 1290), '1', 'no excuses!!')
-#     """
-
-
 
 def render_index(request):
     """renders the site index"""
@@ -34,3 +19,13 @@ def render_index(request):
         'trap_scorekeeping/index.html',
         template_data
     )
+
+
+def render_score_input(request):
+    """renders the score entry page"""
+    template_data = {
+        'things': 'stuff'
+    }
+    return render(request,
+                  'trap_scorekeeping/score_entry.html',
+                  template_data)
