c036b42482247c3c5936885fe2a62fc9652417b6
Stephen <stevemuller@gmail.com>
2016-09-24 17:01:47 -0700

    stuff

--- trap_scorekeeping/views.py
+++ trap_scorekeeping/views.py
@@ -18,7 +18,6 @@ def render_index(request):
     user_list = logic.return_ten_users()
     # hardcoded username for now
     streaks = logic.calculate_streak_by_user('stephen')
-    print(streaks)
     longest_streak = max(streaks)
     shots = logic.calculate_total_shots_for_user('stephen')
     percent_hit = math.floor(logic.hit_percentage_for_user('stephen') * 100)
@@ -41,7 +40,14 @@ def render_index(request):
 
 def render_score_entry(request, model_id=None):
     """renders the score entry page"""
-    print(str(request))
+    # import ipdb; ipdb.set_trace()
+    if request.method == 'POST':
+        post_data = request.POST
+        string_for_db = clean_query_dict_for_score_entry(post_data)
+        round = models.Round.objects.get(id=model_id)
+        round.score = string_for_db
+        round.save()
+        # return redirect('ack_entry', model_id)
     template_data = {
         'id': model_id,
     }
@@ -52,11 +58,14 @@ def render_score_entry(request, model_id=None):
 
 def render_round_entry(request):
     """renders the round entry page"""
-    form = forms.NewScore(request.POST or None)
-    if form.is_valid():
-        instance = form.save(commit=False)
-        instance.save()
-        return redirect('score_entry', model_id=instance.pk)
+    if request.method == 'POST':
+        form = forms.NewScore(request.POST or None)
+        if form.is_valid():
+            instance = form.save(commit=False)
+            instance.save()
+            return redirect('score_entry', model_id=instance.pk)
+    else:
+        form = forms.NewScore()
     template_data = {
         'form': form
     }
@@ -71,3 +80,15 @@ def render_ack_entry(request, model_id=None):
     # }
     # return render(request, 'trap_scorekeeping/ack_entry.html', template_data)
     pass
+
+def clean_query_dict_for_score_entry(query):
+    r"""Takes a query dict from the post request and sets it up for entry in the DB
+
+    >>> data = {'t6': ['f'], 't4': ['d'], 'csrfmiddlewaretoken': ['ubFsTzaUfULAE7Eoat4Ez3tFQKd1uitwuED0ZdLCsDNpkMT4rG5NvwwSAY9xKCkY'], 't3': ['c'], 't2': ['b']}
+    >>> clean_query_dict_for_score_entry(data)
+    'bcdf'
+    """
+    t_values = ['t' + str(i) for i in range(1, 26)]
+    score = [query[t_val][0] for t_val in t_values if t_val in query]
+    score.sort()
+    return ''.join(score)
